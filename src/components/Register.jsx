fa7de56b37782c89faf9f64661ac1fcfa715e75f
Sanjana <sanjana@example.com>
2025-06-18 13:35:26 +0530

    created and styled registration page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div className='text-red-600'>Register</div>
+  <div className='bg-[#efefef] size-full flex justify-center items-center' >
+    <form action="" className='bg-white w-1/3 h-3/4 flex justify-center items-center flex-col' >
+      <div>
+        <h1>Registration Form</h1>
+        </div>
+      <div>
+         <input type="text"  placeholder='Enter Name' />
+      </div>
+
+            <div>
+         <input type="text" placeholder='Enter User Name' />
+      </div>
+
+            <div>
+         <input type="email"  placeholder='Enter Email' />
+      </div>
+
+            <div>
+         <input type="password"  placeholder='Enter Password' />
+      </div>
+
+            <div>
+         <input type="password"  placeholder='Re-type Password' />
+      </div>
+      <div>
+        <button>Click</button>
+      </div>
+    </form>
+  </div>
   )
 }
 
