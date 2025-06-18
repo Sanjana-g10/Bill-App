8923f3585fb1aa010c2cda62cb1c37d4f1829c03
Sanjana <merupulasanjana464@gmail.com>
2025-06-18 20:00:54 +0530

    updated code

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,33 +1,43 @@
 import React from 'react'
+import { CgNametag } from "react-icons/cg";
+import { CiUser } from "react-icons/ci";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { RiLockPasswordFill } from "react-icons/ri";
 
 const Register = () => {
   return (
   <div className='bg-[#efefef] size-full flex justify-center items-center' >
-    <form action="" className='bg-white w-1/2 flex justify-center items-center flex-col gap-4 px-[800px] py-40'>
-      <div className='bg-red-200 w-full  flex  justify-center items-center '>
+    <form action="" className='bg-white w-1/2 h-2/3 flex justify-center items-center flex-col gap-4 px-[80px] py-40'>
+      <div className= 'w-full  flex  justify-center items-center text-3xl font-bold'>
         <h1>Registration Form</h1>
         </div>
-      <div>
-         <input type="text"  placeholder='Enter Name' />
+      <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
+         <input type="text"  placeholder='Enter Name'  className='w-full outline-none px-4 h-10 '/>
+         <CgNametag />
       </div>
 
-            <div>
-         <input type="text" placeholder='Enter User Name' />
+            <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
+         <input type="text" placeholder='Enter User Name' className='w-full outline-none px-4 h-10' />
+         <CiUser />
       </div>
 
-            <div>
-         <input type="email"  placeholder='Enter Email' />
+            <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
+         <input type="email"  placeholder='Enter Email'className='w-full outline-none px-4 h-10'  />
+         <MdOutlineMailOutline />
       </div>
 
-            <div>
-         <input type="password"  placeholder='Enter Password' />
+            <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
+         <input type="password"  placeholder='Enter Password' className='w-full outline-none px-4 h-10'  />
+         <RiLockPasswordLine />
       </div>
 
-            <div>
-         <input type="password"  placeholder='Re-type Password' />
+            <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
+         <input type="password"  placeholder='Re-type Password' className='w-full outline-none px-4 h-10' />
+         <RiLockPasswordFill />
       </div>
-      <div>
-        <button>Click</button>
+      <div className='bg-black text-amber-50 w-full  flex  justify-center items-center rounded-sm '>
+        <button className='w-full outline-none px-4 h-10' >Click</button>
       </div>
     </form>
   </div>
